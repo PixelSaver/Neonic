cbd13aedc7a32f6407a8b0b6a381e3c21ed39497
PixelSaver <stephen.k@proton.me>
2026-03-06 15:42:20 -0800

    Boxline scales target to match the animation

--- scenes/ui/utilities/box_line.gd
+++ scenes/ui/utilities/box_line.gd
@@ -14,21 +14,20 @@ class_name BoxLine
 	set(v):
 		flip_h = v
 		_update_points()
-var og_max_y : float = 0.0
 
 func _ready() -> void:
 	_update_points()
-	og_max_y = target.size.y
+	target.pivot_offset_ratio = Vector2.ONE * 0.5
 
 func _update_points():
 	if not target:
 		return
 	
 	if !Engine.is_editor_hint(): 
-		pass
+		target.scale.y = outline_proportion.y
 	
 	var flip = -1 if flip_h else 1
-	global_position = target.global_position + target.size * Vector2(outline_proportion.x * 0.5 * flip, 0.5) + target.size*Vector2(1,0)*(1 if flip_h else 0)
+	global_position = target.global_position + target.size * Vector2(outline_proportion.x * 0.5 * flip, 0.5 * target.scale.y) + target.size*Vector2(1,0)*(1 if flip_h else 0)
 	self.closed = true
 	var rect = target.size
 	var w = rect.x
