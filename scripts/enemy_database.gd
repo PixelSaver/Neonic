461cd05f95337754b1e6e6b8a3ef785266f74741
PixelSaver <stephen.k@proton.me>
2026-04-13 16:56:35 -0700

    Added heart enemy to room

--- scripts/enemy_database.gd
+++ scripts/enemy_database.gd
@@ -4,6 +4,7 @@ var scenes = {
 	Enemy.Types.BUG : preload("res://scenes/enemies/enemy.tscn"),
 	Enemy.Types.GUNNER : preload("res://scenes/enemies/gunner/gunner_enemy.tscn"),
 	Enemy.Types.CENTIPEDE : preload("res://scenes/enemies/centipede/enemy_centipede.tscn"),
+	Enemy.Types.HEART : preload("res://scenes/enemies/heart/enemy_heart.tscn"),
 }
 func get_enemy_scene(type:Enemy.Types) -> PackedScene:
 	if scenes.has(type):
