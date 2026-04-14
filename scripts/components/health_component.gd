72ab793bbf28f493a6610c9f12793f93b6734652
PixelSaver <stephen.k@proton.me>
2026-04-13 19:08:44 -0700

    Balanced centipede and fixed a little of the procedural animation

--- scripts/components/health_component.gd
+++ scripts/components/health_component.gd
@@ -16,6 +16,9 @@ signal death()
 @export var target : RigidBody2D
 var _target : RigidBody2D
 
+var _pending_atks : Array[Attack] = []
+var _processing := false
+
 func _ready() -> void:
 	if target != null:
 		_target = target
@@ -26,11 +29,23 @@ func _ready() -> void:
 		push_error("Health component couldn't find target.")
 
 func damage(atk: Attack):
-	health -= atk.damage
-	if _target.has_node(^"KnockbackComponent"):
-		var kb_c = _target.get_node(^"KnockbackComponent") as KnockbackComponent
-		kb_c.apply_knockback(atk)
-	if health < 0:
+	_pending_atks.append(atk)
+	if not _processing:
+		_processing = true
+		call_deferred("_process_pending_damage")
+
+func _process_pending_damage() -> void:
+	var tot_damage := 0.0
+	
+	for atk in _pending_atks:
+		tot_damage += atk.damage
+		if _target.has_node(^"KnockbackComponent"):
+			var kb_c = _target.get_node(^"KnockbackComponent") as KnockbackComponent
+			kb_c.apply_knockback(atk)
+	_pending_atks.clear()
+	_processing = false
+	health -= tot_damage
+	if health <= 0 or is_equal_approx(health, 0.0):
 		death.emit()
 
 func heal(delta:float) -> float:
